5bdd24a183c6565a60710f9cec54c60beb738a43
Varnith <varnithdramesh@gmail.com>
2026-02-05 06:03:51 +0000

    added domain entities

--- backend/src/main/java/com/progressive/banking/moneytransfer/MoneytransferApplication.java
+++ backend/src/main/java/com/progressive/banking/moneytransfer/MoneytransferApplication.java
@@ -1,4 +1,4 @@
-package com.fid.moneytransfer;
+package com.progressive.banking.moneytransfer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
