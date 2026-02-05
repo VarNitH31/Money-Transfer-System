c674427dcfa5745667e3ecd4d04330da5f00b548
veeksham22 <veeksham22@gmail.com>
2026-02-05 09:47:33 +0000

    exception folder code dpne

--- backend/src/main/java/com/progressive/banking/moneytransfer/exception/DuplicateTransferException.java
+++ backend/src/main/java/com/progressive/banking/moneytransfer/exception/DuplicateTransferException.java
@@ -1,5 +1,7 @@
 package com.progressive.banking.moneytransfer.exception;
 
-public class DuplicateTransferException {
-
+public class DuplicateTransferException extends RuntimeException {
+    public DuplicateTransferException(String message) {
+        super(message);
+    }
 }
