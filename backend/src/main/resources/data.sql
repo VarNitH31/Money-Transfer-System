b8a59a532e5894d3fce757bcb5924550864ff292
Varnith <varnithdramesh@gmail.com>
2026-02-11 08:40:57 +0000

    Added more dummy acccounts and users

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -8,6 +8,21 @@ VALUES (102, 'Vijay', 5000.00, 'ACTIVE', 0, NOW())
 ON DUPLICATE KEY UPDATE holder_name=VALUES(holder_name), balance=VALUES(balance),
 status=VALUES(status), last_updated=NOW();
 
+INSERT INTO accounts (account_id, holder_name, balance, status, version, last_updated)
+VALUES (104, 'Veeksha', 5000.00, 'ACTIVE', 0, NOW())
+ON DUPLICATE KEY UPDATE holder_name=VALUES(holder_name), balance=VALUES(balance),
+status=VALUES(status), last_updated=NOW();
+
+INSERT INTO accounts (account_id, holder_name, balance, status, version, last_updated)
+VALUES (105, 'Varnith', 500000.00, 'ACTIVE', 0, NOW())
+ON DUPLICATE KEY UPDATE holder_name=VALUES(holder_name), balance=VALUES(balance),
+status=VALUES(status), last_updated=NOW();
+
+INSERT INTO accounts (account_id, holder_name, balance, status, version, last_updated)
+VALUES (106, 'Random', 5000.00, 'ACTIVE', 0, NOW())
+ON DUPLICATE KEY UPDATE holder_name=VALUES(holder_name), balance=VALUES(balance),
+status=VALUES(status), last_updated=NOW();
+
 -- Use LOCKED or CLOSED instead of INACTIVE
 INSERT INTO accounts (account_id, holder_name, balance, status, version, last_updated)
 VALUES (103, 'Inactive User', 2000.00, 'LOCKED', 0, NOW())
@@ -21,4 +36,16 @@ ON DUPLICATE KEY UPDATE password = VALUES(password);
  
 INSERT INTO users (username, password)
 VALUES ('Kaushik', '$2a$10$psGIy2yLxsiME1SsJtsDtOtPSCj596.NmMrZW/BKDmxyBwRDlQmGe')
+ON DUPLICATE KEY UPDATE password = VALUES(password);
+
+INSERT INTO users (username, password)
+VALUES ('Veeksha', '$2a$10$psGIy2yLxsiME1SsJtsDtOtPSCj596.NmMrZW/BKDmxyBwRDlQmGe')
+ON DUPLICATE KEY UPDATE password = VALUES(password);
+
+INSERT INTO users (username, password)
+VALUES ('Varnith', '$2a$10$psGIy2yLxsiME1SsJtsDtOtPSCj596.NmMrZW/BKDmxyBwRDlQmGe')
+ON DUPLICATE KEY UPDATE password = VALUES(password);
+
+INSERT INTO users (username, password)
+VALUES ('Random', '$2a$10$psGIy2yLxsiME1SsJtsDtOtPSCj596.NmMrZW/BKDmxyBwRDlQmGe')
 ON DUPLICATE KEY UPDATE password = VALUES(password);
